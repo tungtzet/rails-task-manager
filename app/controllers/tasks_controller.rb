61369c4b26d4773523e1a697e6f65930524d9bca
tung nguyen <tungnt@arena.edu.vn>
2020-07-28 13:05:06 +0200

    edit task done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,12 +2,15 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
   def show
     @task = Task.find(params[:id])
   end
+
   def new
     @task = Task.new
   end
+
   def create
     @task = Task.new(task_params)
     @task.save
@@ -15,9 +18,19 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Show all tasks
   get "tasks", to: "tasks#index"
+
+  # Create a new task
   get "tasks/new", to: "tasks#new"
-  get "tasks/:id", to: "tasks#show", as: :task
   post "tasks", to: "tasks#create"
+
+  # Show a task
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # Update a task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
 end
